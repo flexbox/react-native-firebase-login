a4c59f2cdb5545dd7ccdea09868fba4bb6572f10
David Leuliette <david.leuliette@smartpension.co.uk>
2017-09-06 20:17:25 +0100

    Copying Reusable Components and allow to export all the thing

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Header } from './src/common';
 
 class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
+        <Header text="Authentication" />
         <Text>Hello World</Text>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
---
+++ src/common/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+  return (
+    <View style={styles.row}>
+      <Text style={styles.title}>{props.text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: {
+    backgroundColor: '#F3FEF6',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700'
+  }
+});
+
+// Export All the thing
+export { Header };
