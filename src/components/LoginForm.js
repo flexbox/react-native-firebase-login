abc009d522b42abe135f1c7a7e3d0ee4d77c9baf
David Leuliette <david.leuliette@smartpension.co.uk>
2017-09-06 22:07:11 +0100

    Adding user input with TextInput

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, TextInput } from 'react-native';
 import { Card, CardSection } from './common';
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardSection />
+        <CardSection>
+          <TextInput style={{ height: 20, width: 100 }}/>
+        </CardSection>
         <CardSection />
 
         <CardSection>
           <Button title="Signin" />
         </CardSection>
-
       </Card>
     );
   }
