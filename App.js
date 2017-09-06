e0704a9d24201ad954a7ef805c49030ab58ff4ec
David Leuliette <david.leuliette@smartpension.co.uk>
2017-09-06 21:19:14 +0100

    Add firebase credentials

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './src/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+       apiKey: "AIzaSyC-l3sXNnZQgDWAkAeXuUZzssiiRT0FgCU",
+       authDomain: "simple-react-native-login.firebaseapp.com",
+       databaseURL: "https://simple-react-native-login.firebaseio.com",
+       projectId: "simple-react-native-login",
+       storageBucket: "simple-react-native-login.appspot.com",
+       messagingSenderId: "97294280684"
+    });
+  }
+
   render() {
     return (
       <View>
