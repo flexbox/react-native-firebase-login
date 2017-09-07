0f2a5cff059842315bb7227b6773017e953ce642
David Leuliette <david.leuliette@smartpension.co.uk>
2017-09-07 10:49:19 +0100

    Add error handling if the email and password are wrong

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { StyleSheet, Text } from 'react-native';
 import firebase from 'firebase';
 import { Button } from 'react-native';
 import { Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    firebase.auth().signInWithEmailAndPassword(email, password);
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+          firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(() => {
+              this.setState({ error: 'Authentication failed.' });
+            });
+      });
   }
 
   render() {
@@ -34,13 +41,24 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.textError}>
+          {this.state.error}
+        </Text>
+
         <Button
           onPress={this.onButtonPress.bind(this)}
           title="Signin" />
-
       </Card>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textError: {
+    color: 'red',
+    alignSelf: 'center',
+    paddingVertical: 20
+  }
+});
 
 export default LoginForm;
